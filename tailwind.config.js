facac4059a761aaff555a3503b517f4a5ad61923
Manu <manuvaglz@gmail.com>
2024-03-16 18:09:45 -0600

    IULA-0004 feat: Format

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    colors: {
+      white: 'FFFFFF',
+      primary: '#012649',
+      secondary: '#113353',
+      accent: '#1FC667',
+      accent2: '#00B2A4'
+    },
+    extend: {
+      backgroundImage: (theme) => ({
+        'gradient-primary': `linear-gradient(to right, ${theme('colors.accent')}, ${theme('colors.accent2')})`,
+      }),
+  },
   },
   plugins: [],
 }
