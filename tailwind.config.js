dc9a942ab7a1b9f265f9a09182090ad87d57b6bb
Manu <manuvaglz@gmail.com>
2024-03-16 19:59:44 -0600

    IULA-0005 feat: Hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,23 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     colors: {
-      white: 'FFFFFF',
+      white: '#FFFFFF',
       primary: '#012649',
       secondary: '#113353',
       accent: '#1FC667',
       accent2: '#00B2A4'
     },
+    fontSize: {
+			default: ['18px', { fontWeight: 400}],
+			bold: ['18px', { fontWeight: 700}],
+			large: ['24px', { fontWeight: 200 }],
+			subtitle: ['24px', {  fontWeight: 700 }],
+      custom: ['46px', { fontWeight: 200}],
+			title: ['64px', { fontWeight: 900}]
+		},
+    fontFamily:{
+			montserrat: ['Montserrat','sans-serif'],
+		},
     extend: {
       backgroundImage: (theme) => ({
         'gradient-primary': `linear-gradient(to right, ${theme('colors.accent')}, ${theme('colors.accent2')})`,
