e02664a4333910d545400d3563b3b68f918ca5dc
Manu <manuvaglz@gmail.com>
2024-04-06 12:47:57 -0600

    IULA-0020 feat: RRSS

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,12 @@
-const Button = ({text, classes = '', type = '', href = ''}) => {
-  return <a className={`bg-gradient-primary p-4 rounded text-bold md:text-subtitle text-center text-white hover:scale-105 transition-all duration-500 ${classes}`} type={type} href={href}>{text}</a>
+const Button = ({text, classes = '', type = '', href = '', target = ''}) => {
+  return <a 
+    className={`bg-gradient-primary p-4 rounded text-bold md:text-subtitle text-center text-white hover:scale-105 transition-all duration-500 ${classes}`} 
+    type={type} 
+    href={href}
+    target={target}
+  >
+    {text}
+  </a>
 }
 
 export default Button
